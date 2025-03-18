5b587a5e8cf3a93c4cade68dc1ee5e4d1e189cbe
Luanwkb <168366309+LuanSpamaIfElse@users.noreply.github.com>
2025-03-18 19:59:03 -0300

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -32,25 +32,61 @@ class Player(pygame.sprite.Sprite):
         self.x_change = 0
         self.y_change = 0
         self.facing = 'down'
-
         # Carrega a imagem do personagem
-        self.image = self.game.caracter_spritesheet.get_sprite(3, 2, 38, 40)
+        #self.image = self.game.character_spritesheet.get_sprite(1, 3, 38, 39)
+        self.animation_frames = {
+    'left': [
+        self.game.character_spritesheet.get_sprite(1, 42, self.width+1, 39 ),
+        #self.game.character_spritesheet.get_sprite(1, 60, 28, 38)
+    ],
+    'right': [
+        self.game.character_spritesheet.get_sprite(33, 42, self.width+1, 39),
+    ],
+    'up': [
+        self.game.character_spritesheet.get_sprite(79, 4, self.width, self.height+1),
+    ],
+    'down': [
+        self.game.character_spritesheet.get_sprite(1, 4, self.width, self.height+1),
+    ]
+}
 
-        # Define o retângulo de colisão
+        self.current_frame = 0
+        self.animation_speed = 1
+        self.animation_counter = 0
+
+        self.image = self.animation_frames[self.facing][self.current_frame]
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def animate(self):
+        # Alterna entre os frames de animação
+        self.animation_counter += 1
+        if self.animation_counter >= self.animation_speed:
+            self.animation_counter = 0
+            self.current_frame = (self.current_frame + 1) % len(self.animation_frames[self.facing])
+            self.image = self.animation_frames[self.facing][self.current_frame]
+        
+        x = self.rect.x
+        y = self.rect.y
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
 
     def update(self):
         self.movement()
+        
 
         # Aplica o movimento
         self.rect.x += self.x_change
         self.collide_blocks('x')
+        self.x = self.rect.x
 
         self.rect.y += self.y_change
         self.collide_blocks('y')
-
+        self.y = self.rect.y
+        self.animate()
         # Reseta os valores de movimento após cada frame
         self.x_change = 0
         self.y_change = 0
@@ -108,6 +144,80 @@ class Ground(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
+
+class enemy(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.game = game
+        self._layer = ENEMY_LAYER
+        self.groups = self.game.all_sprites, self.game.enemies
+        pygame.sprite.Sprite.__init__(self, self.groups)
+
+        self.x = x * TILESIZES
+        self.y = y * TILESIZES
+        self.width = TILESIZES
+        self.height = TILESIZES
+
+        self.x_change = 0
+        self.y_change = 0
+        self.facing = random.choice(['left', 'right'])
+        self.movement_loop = 0  # Inicialização correta da variável
+        self.max_travel = random.randint(7, 30)
+
+        # Animação
+        self.animation_frames = {
+            'left': [
+                self.game.enemy_spritesheet.get_sprite(3, 98, self.width, self.height),
+                self.game.enemy_spritesheet.get_sprite(35, 98, self.width, self.height),
+                self.game.enemy_spritesheet.get_sprite(68, 98, self.width, self.height)
+            ],
+            'right': [
+                self.game.enemy_spritesheet.get_sprite(3, 66, self.width, self.height),
+                self.game.enemy_spritesheet.get_sprite(35, 66, self.width, self.height),
+                self.game.enemy_spritesheet.get_sprite(68, 66, self.width, self.height)
+           ]
+        }
+        self.current_frame = 0
+        self.animation_speed = 10
+        self.animation_counter = 0
+
+        self.image = self.animation_frames[self.facing][self.current_frame]
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def update(self):
+        self.movement()
+        self.animate()  # Atualiza a animação
+        self.rect.x += self.x_change
+        self.rect.y += self.y_change
+
+        self.x_change = 0
+        self.y_change = 0
+
+    def movement(self):
+        if self.facing == 'left':
+            self.x_change -= ENEMY_SPEED
+            self.movement_loop -= 1
+            if self.movement_loop <= -self.max_travel:
+                self.facing = 'right'
+
+        if self.facing == 'right':
+            self.x_change += ENEMY_SPEED
+            self.movement_loop += 1
+            if self.movement_loop >= self.max_travel:
+                self.facing = 'left'
+
+    def animate(self):
+        # Alterna entre os frames de animação
+        self.animation_counter += 1
+        if self.animation_counter >= self.animation_speed:
+            self.animation_counter = 0
+            self.current_frame = (self.current_frame + 1) % len(self.animation_frames[self.facing])
+            self.image = self.animation_frames[self.facing][self.current_frame]
+
+
+        
 
 
 class Block(pygame.sprite.Sprite):
