ea23b77cbc7f46b15ba3316227b448d804a64716
Luanwkb <168366309+LuanSpamaIfElse@users.noreply.github.com>
2025-04-03 06:23:41 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 WIN_WIDTH = 1020
 WIN_HEIGHT = 660
 TILESIZES = 32
-ENEMY_COUNT = 10
-OBSTACLE_COUNT = 5
+ENEMY_COUNT = 6
+OBSTACLE_COUNT = 8
 
 PLAYER_LAYER = 5
 OBSTACLE_LAYER = 4
@@ -10,8 +10,19 @@ ENEMY_LAYER = 3
 BLOCK_LAYER = 2
 GROUND_LAYER = 1
 
+PLAYER_LIFE = 15
+ENEMY_LIFE = 3
+
 PLAYER_SPEED = 6
 ENEMY_SPEED = 2
+
+# Barra de cooldown do ataque
+ATTACK_BAR_WIDTH = 100
+ATTACK_BAR_HEIGHT = 15
+ATTACK_BAR_X = 10
+ATTACK_BAR_Y = 30  # Abaixo da barra de dodge (se você quiser manter ambas)
+ATTACK_BAR_COLOR = (255, 0, 0)   
+ATTACK_COOLDOWN_COLOR = (0, 255, 0)  
 
 # Cores da barra de dodge
 DODGE_BAR_WIDTH = 100
@@ -23,14 +34,22 @@ DODGE_COOLDOWN_COLOR = (255, 0, 0)  # Vermelho durante cooldown
 DODGE_BAR_BG_COLOR = (50, 50, 50)   # Fundo cinza
 
 # Cooldowns das habilidades
-ATTACK_COOLDOWN = 3000  # 3 segundos em milissegundos
+ATTACK_COOLDOWN = 1000  # 1 segundos em milissegundos
 DODGE_COOLDOWN = 3000   # 3 segundos em milissegundos
 
+GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 BLACK = (0,0, 0)
 FPS = 25
+
+HEALTH_BAR_WIDTH = 40
+HEALTH_BAR_HEIGHT = 5
+HEALTH_BAR_OFFSET = 10  # Distância acima do sprite
+PLAYER_HEALTH_COLOR = (0, 255, 0)  # Verde
+ENEMY_HEALTH_COLOR = (255, 0, 0)   # Vermelho
+HEALTH_BAR_BG_COLOR = (50, 50, 50) # Cinza escuro
 
 # Painel de Habilidades
 ABILITY_PANEL_WIDTH = 300
@@ -40,12 +59,24 @@ ABILITY_PANEL_Y = 10
 ABILITY_PANEL_COLOR = (50, 50, 50, 200)  # Cinza escuro semi-transparente
 ABILITY_TEXT_COLOR = WHITE
 
+# Diálogo
+DIALOG_BOX_WIDTH = WIN_WIDTH - 40
+DIALOG_BOX_HEIGHT = 140  # Aumentei a altura para acomodar o texto adicional
+DIALOG_BOX_X = 20
+DIALOG_BOX_Y = WIN_HEIGHT - DIALOG_BOX_HEIGHT - 20
+DIALOG_BOX_COLOR = (50, 50, 50)
+DIALOG_TEXT_COLOR = WHITE
+DIALOG_FONT_SIZE = 24
+DIALOG_SMALL_FONT_SIZE = 18  # Adicionei para o texto "APERTE ESPAÇO"
+DIALOG_TEXT_MARGIN = 15
+DIALOG_TEXT_SPEED = 2
+
 #fases
 tilemap = [
     'BBBBBBBBBBBBBBBBBBBBBBBBBB',
     'B........................B',
     'B........................B',
-    'B.....O..........Q.......B',
+    'B................Q.......B',
     'B........................B',
     'B........................B',
     'B...........Q............B',
